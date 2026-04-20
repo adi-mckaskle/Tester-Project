47a8bfda62db7e9decd13d9d8a2324cf6926fb90
nerdynoodle24 <maddymaeflower@gmail.com>
2026-04-20 18:49:32 -0400

    Inventory System Progress
    Progress on inventory

--- Assets/Maddy's Scripts/InventoryController.cs
+++ Assets/Maddy's Scripts/InventoryController.cs
@@ -6,14 +6,14 @@ public class InventoryController : MonoBehaviour
     public GameObject slotPrefab;
     public int slotCount;
     public GameObject[] itemPrefabs;
-   
-    // Start is called before first frame update
+
+    // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < slotCount; i++)
+        for(int i = 0; i < slotCount; i++)
         {
             Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
-            if (i < itemPrefabs.Length)
+            if(i < itemPrefabs.Length)
             {
                 GameObject item = Instantiate(itemPrefabs[i], slot.transform);
                 item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
