63c70e0509420e5061fdc605402d4ce617a5a9e7
nerdynoodle24 <maddymaeflower@gmail.com>
2026-05-03 12:53:32 -0400

    Readded npc dialogue system

---
+++ Assets/Maddy's Scripts/NPC.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public NPCDialogue dialogueData;
+    private DialogueController dialogueUI;
+    private int dialogueIndex;
+    private bool isTyping;
+
+    private void Start()
+    {
+        dialogueUI = DialogueController.Instance;
+    }
+
+    public void StartDialogue()
+    {
+        dialogueIndex = 0;
+
+        dialogueUI.SetNPCInfo(dialogueData.npcName, dialogueData.npcPortrait);
+        dialogueUI.ShowDialogueUI(true);
+
+        DisplayCurrentLine();
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            // Skip typing animation and show the full line
+            StopAllCoroutines();
+            dialogueUI.SetDialogueText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+
+        //Clear Choices
+        dialogueUI.ClearChoices();
+
+        //Check endDialogueLines
+        if(dialogueData.endDialogueLines.Length > dialogueIndex && dialogueData.endDialogueLines[dialogueIndex])
+        {
+            EndDialogue();
+            return;
+        }
+
+        //Check if choices & display
+        foreach(DialogueChoice dialogueChoice in dialogueData.choices)
+        {
+            if(dialogueChoice.dialogueIndex == dialogueIndex)
+            {
+                DisplayChoices(dialogueChoice);
+                return;
+            }
+        }
+
+        if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            //If another line, type next line
+            DisplayCurrentLine();
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueUI.SetDialogueText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueUI.SetDialogueText(dialogueUI.dialogueText.text += letter);
+            //SoundEffectManager.PlayVoice(dialogueData.voiceSound, dialogueData.voicePitch);
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+
+    void DisplayChoices(DialogueChoice choice)
+    {
+        for(int i = 0; i < choice.choices.Length; i++)
+        {
+            int nextIndex = choice.nextDialogueIndexes[i];
+            dialogueUI.CreateChoiceButton(choice.choices[i], () => ChooseOption(nextIndex));
+        }
+    }
+
+    void ChooseOption(int nextIndex)
+    {
+        dialogueIndex = nextIndex;
+        dialogueUI.ClearChoices();
+        DisplayCurrentLine();
+    }
+
+    void DisplayCurrentLine()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TypeLine());
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        dialogueUI.SetDialogueText("");
+        dialogueUI.ShowDialogueUI(false);
+    }
+}
