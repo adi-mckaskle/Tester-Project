c7224666a6221cdef8575131c84a359a4da71ddf
nerdynoodle24 <maddymaeflower@gmail.com>
2026-05-04 23:32:34 -0400

    Update RewardsController.cs

--- Assets/Maddy's Scripts/RewardsController.cs
+++ Assets/Maddy's Scripts/RewardsController.cs
@@ -11,45 +11,46 @@ public class RewardsController : MonoBehaviour
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
-
-    public void GiveQuestReward(Quest quest)
-    {
-        if (quest?.questRewards == null) return;
-
-        foreach(var reward in quest.questRewards)
-        {
-            switch (reward.type)
-            {
-                case RewardType.Item:
-                    GiveItemReward(reward.rewardID, reward.amount);
-                    break;
-                case RewardType.Gold:
-                    break;
-                case RewardType.Experience:
-                    break;
-                case RewardType.Custom:
-                    break;
-            }
-        }
-    }
-
-    public void GiveItemReward(int itemID, int amount)
-    {
-        var itemPrefab = FindAnyObjectByType<ItemDictionary>()?.GetItemPrefab(itemID);
-
-        if (itemPrefab == null) return;
-
-        for(int i = 0; i < amount; i++)
-        {
-            if(!InventoryController.Instance.AddItem(itemPrefab))
-            {
-                //    GameObject dropItem = Instantiate(itemPrefab, transform.position + Vector3.down, Quaternion.identity);
-                //    dropItem.GetComponent<BounceEffect>().StartBounce();
-                //}
-                //else
-                //{
-                itemPrefab.GetComponent<Item>();
-            }
-        }
-    }
 }
+
+//    public void GiveQuestReward(Quest quest)
+//    {
+//        if (quest?.questRewards == null) return;
+
+//        foreach(var reward in quest.questRewards)
+//        {
+//            switch (reward.type)
+//            {
+//                case RewardType.Item:
+//                    GiveItemReward(reward.rewardID, reward.amount);
+//                    break;
+//                case RewardType.Gold:
+//                    break;
+//                case RewardType.Experience:
+//                    break;
+//                case RewardType.Custom:
+//                    break;
+//            }
+//        }
+//    }
+
+//    public void GiveItemReward(int itemID, int amount)
+//    {
+//        var itemPrefab = FindAnyObjectByType<ItemDictionary>()?.GetItemPrefab(itemID);
+
+//        if (itemPrefab == null) return;
+
+//        for(int i = 0; i < amount; i++)
+//        {
+//            if(!InventoryController.Instance.AddItem(itemPrefab))
+//            {
+//                //    GameObject dropItem = Instantiate(itemPrefab, transform.position + Vector3.down, Quaternion.identity);
+//                //    dropItem.GetComponent<BounceEffect>().StartBounce();
+//                //}
+//                //else
+//                //{
+//                itemPrefab.GetComponent<Item>();
+//            }
+//        }
+//    }
+//}
