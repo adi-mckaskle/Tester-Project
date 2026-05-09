36cc0aecc53bdc1b715e1c3600037550c0cfd307
nerdynoodle24 <maddymaeflower@gmail.com>
2026-05-09 18:01:30 -0400

    Created Inventory Manager & Inventory UI
    I split the inventory controller into two scripts, the manager for data and UI f

--- Assets/Maddy's Scripts/InventoryUI.cs
+++ Assets/Maddy's Scripts/InventoryUI.cs
@@ -1,16 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject slotPrefab;
+    public Transform inventoryPanel;
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.OnInventoryUpdated += RefreshUI;
+        }
+    }
+    private void OnDisable()
+    {
+        // 2. Unsubscribe to prevent "Ghost" errors
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.OnInventoryUpdated -= RefreshUI;
+            RefreshUI();
+        }
+    }
+    public void RefreshUI()
+    {
+        // The Clean Slate
+        foreach (Transform child in inventoryPanel)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < InventoryManager.Instance.slots.Count; i++)
+        {
+            InventorySlot slotData = InventoryManager.Instance.slots[i];
+            GameObject newSlot = Instantiate(slotPrefab, inventoryPanel);
+
+            // If the data says there's an item, draw it
+            if (!slotData.IsEmpty)
+            {
+                DrawItemInSlot(slotData, newSlot.transform);
+            }
+        }
+    }
+    private void DrawItemInSlot(InventorySlot data, Transform slotTransform)
+    {
+        // 7. Get the visual prefab from the Dictionary
+        GameObject prefab = ItemDictionary.Instance.GetItemPrefab(data.itemID);
+
+        if (prefab != null)
+        {
+            GameObject visualItem = Instantiate(prefab, slotTransform);
+
+            // 8. Update the item's internal quantity
+            Item itemScript = visualItem.GetComponent<Item>();
+            if (itemScript != null)
+            {
+                itemScript.quantity = data.quantity;
+            }
+        }
     }
 }
