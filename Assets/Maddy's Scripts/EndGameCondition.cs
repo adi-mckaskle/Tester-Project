8562a60a2572369699dbc1e44332286288aa3e87
nerdynoodle24 <maddymaeflower@gmail.com>
2026-05-08 22:33:33 -0400

    Hegdes

--- Assets/Maddy's Scripts/EndGameCondition.cs
+++ Assets/Maddy's Scripts/EndGameCondition.cs
@@ -4,16 +4,4 @@ using UnityEngine.SceneManagement;
 public class EndGameCondition : MonoBehaviour
 {
     private ItemDictionary itemDictionary;
-    //[SerializeField] private string requiredItemName = "Name";
-    [SerializeField] private string requiredItemName = "Reward";
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            //Access the player's inventory to check for the item
-            InventoryController inventoryController = other.GetComponent<InventoryController>();
-
-
-        }
-    }
 }
