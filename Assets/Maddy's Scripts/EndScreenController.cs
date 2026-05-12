d463e81c7e0c38b796ef5fdc5a0d23f0856376fa
nerdynoodle24 <maddymaeflower@gmail.com>
2026-05-12 11:39:30 -0400

    Got the End Menu and Main Menu working

--- Assets/Maddy's Scripts/EndScreenController.cs
+++ Assets/Maddy's Scripts/EndScreenController.cs
@@ -3,19 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class EndScreenController : MonoBehaviour
 {
-    public void RestartLevel()
+    public void GoToMainMenu()
     {
         Time.timeScale = 1f;
 
-        //if (InventoryManager.Instance != null) // 5
-        //{
-        //    InventoryManager.Instance.ResetInventory(); // 6
-        //}
-        //if (QuestController.Instance != null)
-        //{
-        //    QuestController.Instance.ResetQuest();
-        //}
-
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
+    
+    //public void RestartLevel()
+    //{
+    //    Time.timeScale = 1f;
+
+    //    if (InventoryManager.Instance != null) // 5
+    //    {
+    //        InventoryManager.Instance.ResetInventory(); // 6
+    //    }
+    //    if (QuestController.Instance != null)
+    //    {
+    //        QuestController.Instance.ResetQuest();
+    //    }
+
+    //    SceneManager.LoadScene(1);
+    //}
 }
